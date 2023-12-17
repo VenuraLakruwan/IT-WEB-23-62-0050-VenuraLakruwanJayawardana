3e3556b1e7db7ce765925b3ec34aa493f16b7dfc
VenuraLakruwan <venura.l.jayawardana@gmail.com>
2023-12-18 00:54:45 +0530

    Q2 & Q1

---
+++ Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace Q2.Models
+{
+    public interface Course
+    {
+        public int CourseID { get; set; }
+        public string  Name { get; set; }
+        public string LecturerName { get; set; }
+    }
+}
+    
---
+++ Models/students.cs
@@ -0,0 +1,12 @@
+﻿namespace Q2.Models
+{
+    public class students
+    {
+        public int StudentID { get; set; }
+        public String  Name { get; set; }
+
+        public string  City { get; set; }
+
+        public int CourseID { get; set; }
+    }
+}
